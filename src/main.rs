81cff8286a877972f6947dba780592c31188ba3b
Alex <alex@hitch>
2022-11-12 18:24:43 +0100

    Update tests and CI config

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@
 
 use std::net::TcpListener;
 
+use zero2prod::configuration;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    let configuration = configuration::get_configuration().expect("Failed to read configuration");
+
+    //let listener = TcpListener::bind("127.0.0.1:8000")?;
+    let host_port = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(host_port)?;
+
     zero2prod::startup::run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
@@ -20,6 +22,13 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let address = spawn_app();
 
+    let configuration =
+        zero2prod::configuration::get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
+
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=urusla_le_guin%40gmail.com";
@@ -33,6 +42,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guid@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
