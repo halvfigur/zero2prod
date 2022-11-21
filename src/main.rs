3534b34477fbe1b3a88ddc087d31c66f9810e45b
Alex <alex@hitch>
2022-11-21 18:01:43 +0100

    Move telemetry to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 //! main.rs
 
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration;
+use zero2prod::telemetry;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into());
+
+    telemetry::init_subscriber(subscriber);
 
     let configuration = configuration::get_configuration().expect("Failed to read configuration");
 
-    //let listener = TcpListener::bind("127.0.0.1:8000")?;
-    let host_port = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(host_port)?;
-    //let connection = PgConnection::connect(&configuration.database.connection_string())
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
+
+    let host_port = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(host_port)?;
 
     zero2prod::startup::run(listener, connection)?.await
 }
