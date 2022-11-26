c63738f6dc294c59afff99a1e9bf3dd476e90c2b
Alex <alex@hitch>
2022-11-26 10:50:26 +0100

    Add instrumetation

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,27 @@
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
-use zero2prod::configuration::{self, DatabaseSettings};
+use zero2prod::{
+    configuration::{self, DatabaseSettings},
+    telemetry,
+};
+
+use once_cell::sync::Lazy;
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber =
+            telemetry::get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        telemetry::init_subscriber(subscriber);
+    } else {
+        let subscriber =
+            telemetry::get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        telemetry::init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     address: String,
@@ -82,6 +102,8 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{port}");
@@ -90,15 +112,11 @@ async fn spawn_app() -> TestApp {
         configuration::get_configuration().expect("Failed to read configuration");
     configuration.database.database_name = Uuid::new_v4().to_string();
 
-    /*
-    let db_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
-        */
     let db_pool = configure_database(&configuration.database).await;
 
     let server =
         zero2prod::startup::run(listener, db_pool.clone()).expect("Failed to bind address");
+
     let _ = tokio::spawn(server);
 
     TestApp { address, db_pool }
